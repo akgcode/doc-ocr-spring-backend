ad66f05a81cdbb0dd28d778250dc8e3250cf8fce
akgcode <avdhesh.kumar1221@gmail.com>
2026-01-01 02:26:23 +0530

    boilerplate code

---
+++ src/main/java/com/akg/doc_ocr_spring_backend/DocOcrSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.akg.doc_ocr_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocOcrSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocOcrSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akg/doc_ocr_spring_backend/DocOcrSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akg.doc_ocr_spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocOcrSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
